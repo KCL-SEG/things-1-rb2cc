0e60dfe73eca25410c117121962eec9154fb4272
Ricky Brown <ricky.brown@kcl.ac.uk>
2022-10-20 04:17:41 +0100

    Update models.py

--- things/models.py
+++ things/models.py
@@ -1,10 +1,10 @@
-from django.db import models
+from django.db.models.Model import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 
-class Thing(models):
+class Thing(models.Model):
     name = models.CharField(
         max_length=30, unique=True, blank=False
     )
