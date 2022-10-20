b710dbbd3545a01fe80f9ea6495e355cade522a1
Ricky Brown <ricky.brown@kcl.ac.uk>
2022-10-20 03:58:09 +0100

    Update syntax

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
-class Things(models.Model):
+class Thing(models.Model):
     name = models.CharField(
         max_length=30, unique=True, blank=False
     )
